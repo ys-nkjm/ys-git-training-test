5a6d05b1b74858ab37ea6ce6377ea7a58aab9136
nakajima-ittoku <ittoku.nakajima@ysinc.co.jp>
2023-12-25 12:37:10 +0900

    型をチェックする

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ function fizzBuzz() {
   const val = document.getElementById('input-max-number').value;
   const maxNumber = parseInt(val, 10);
 
+  if(typeof maxNumber !== 'number') {
+    console.log('入力内容は数値にしてください。')
+    return;
+  }
+
   for(let i = 1; i <= maxNumber; i++) {
     if (i % 15 === 0) {
-      console.log('FizzBuzz')
+      console.log('FizzBuzz');
     }
     else if (i % 3 === 0) {
       console.log('Fizz');
     }
     else if (i % 5 === 0) {
-      console.log('Buzz')
+      console.log('Buzz');
     }
     else {
-      console.log(i)
+      console.log(i);
     }
   }
 }
